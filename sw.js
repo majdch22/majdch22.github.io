5baf20eb638e208f0b9b86da6db55ba518685d2d
majdch22 <167249031+majdch22@users.noreply.github.com>
2026-03-22 18:00:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-  self.addEventListener("install", () => self.skipWaiting());
+  self.addEventListener("install", e => {
+    self.skipWaiting();
+    e.waitUntil(
+      self.registration.paymentManager.instruments.set("basic", {
+        name: "Test Pay",
+        method: "https://animated-piroshki-a90b53.netlify.app/pay.json"
+      })
+    );
+  });
+
   self.addEventListener("activate", e => e.waitUntil(clients.claim()));
+
   self.addEventListener("paymentrequest", e => {
     console.log("[SW] Payment handler fired!");
     console.log("[SW] Total:", JSON.stringify(e.total));
