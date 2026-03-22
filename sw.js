7b1a152a05841969ed3e805676b86184ec36d24c
majdch22 <167249031+majdch22@users.noreply.github.com>
2026-03-22 17:28:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
   self.addEventListener("install", () => self.skipWaiting());
   self.addEventListener("activate", e => e.waitUntil(clients.claim()));
   self.addEventListener("paymentrequest", e => {
-    console.log("[SW] fired!", e.total);
+    console.log("[SW] Payment handler fired!");
+    console.log("[SW] Total:", JSON.stringify(e.total));
+    console.log("[SW] Method:", e.methodName);
   });
